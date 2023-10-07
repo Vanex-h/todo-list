5fb10ca7d61d4e010b4b26ad22017acb4a7dc468
Vanex-h <vanessahirwa5@gmail.com>
2023-10-07 23:30:23 +0200

    Did

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../src/App.css'
+import TodoList from './Todolist';
+
+
+const App = () => {
+    return (
+        <div>
+            <TodoList />
+        </div>
+    );
+};
+
+export default App;
